0018d0eeb4b2dd0fa2d44b11a44e292179d88cdf
Adel <ibrahia@ucalgary.ca>
2021-07-07 20:55:56 -0600

    Add axios request for days array

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,33 +1,38 @@
 import React from "react";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "components/Application.scss";
 
 import DayList from "./DayList";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+const axios = require('axios').default;
+
+
 
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
- 
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days')
+    .then(function (response) {
+      // handle success
+      setDays([...response.data]);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+    
+  }, [])
+
+  
+
 
 
   return (
