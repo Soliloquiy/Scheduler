d1443effc55416f5e0fd4e24bff9029f44a9c455
Adel <ibrahia@ucalgary.ca>
2021-07-11 11:34:55 -0600

    Add custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,9 @@ import "components/Appointment/styles.scss";
 import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
+
+import useVisualMode from 'hooks/useVisualMode';
 
 // const classNames = require('classnames');
 
@@ -13,6 +16,13 @@ import Empty from 'components/Appointment/Empty';
 
 export default function Appointment(props) {
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = 'CREATE';
+
+const {mode, transition, back} = useVisualMode(
+  props.interview ? SHOW : EMPTY
+  );
 
 
   // const interviewerClass = classNames("interviewers__item", {
@@ -22,9 +32,29 @@ export default function Appointment(props) {
 
   return (
     <article className="appointment">
-      <Header time={props.time}/>
+      <Header time={props.time} />
 
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} onEdit={props.onEdit} onDelete={props.onDelete} /> : <Empty onAdd={props.onAdd} />}
+      {mode === EMPTY && <Empty onAdd={() => {
+        return transition(CREATE);
+      }} />
+      }
+
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onEdit={props.onEdit}
+          onDelete={props.onDelete} />
+      )}
+
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={() => { console.log('save') }}
+          onCancel={() => { return back() }} />
+      )}
+
+      {/* {props.interview ?  : } */}
 
 
     </article>
