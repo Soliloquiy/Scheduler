812327bfeeb891355d99c269e2023c88dfa974ee
Adel <ibrahia@ucalgary.ca>
2021-07-14 17:30:59 -0600

    Add Cypress for End-to End Testing

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,25 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+
+    // cy.get("li")
+    //   .contains("Tuesday")
+    //   .click();
+
+    // cy.contains("li", "Tuesday")
+    //   .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+
+    //Above can be refactored to:
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
+
+  
+
+});
